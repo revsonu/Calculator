f700f9d317b65786f8a6724fb85c287469d3c761
sonu meena <kumarsonu123karauli@gmail.com>
2024-01-17 23:55:09 +0530

    added project to repo

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import styles from "./App.module.css";
+import ButtonsContainer from "./components/ButtonsContainer";
+import Display from "./components/Display";
+
+function App() {
+  
+  const [calVal, setcalVal] = useState("");
+
+  const onButtonClick = (buttonText) => {
+    
+    if(buttonText === 'C'){
+      setcalVal("");
+    } else if(buttonText === '='){
+      const result = eval(calVal);
+      setcalVal(result);
+    }
+    else{
+      const newDisplayValue = calVal + buttonText;
+      setcalVal(newDisplayValue);
+    }
+  };
+
+  return (
+    <center>
+      <div className={styles.Calculator}>
+        <Display displayValue={calVal}></Display>
+        <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+      </div>
+    </center>
+  );
+}
+
+export default App;
